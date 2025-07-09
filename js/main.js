69544cda4def149bf2eb899711f8bb1fa69441fe
Song Ye Bin <75508349+enddl3788@users.noreply.github.com>
2025-07-09 14:50:38 +0000

    update

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.tab-pane').forEach(el => {
-    el.classList.add('show', 'active');
-  });
-  document.querySelectorAll('.nav-link').forEach(tab => tab.classList.remove('active'));
-});
